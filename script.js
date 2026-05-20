32a325d630d3949f551299c264abe09650a45a45
vjp-danielIR <diglesiasr04@informatica.iesvalledeljerteplasencia.es>
2026-05-20 11:20:00 +0200

    Add new dinosaur models: Velociraptor and Tiny Planet Friends

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ function shouldUseSoftNavigation() {
   );
 }
 
-function getBodySwapNodes(root) {
+function getBodySwapNodes(root = document.body) {
   return [...root.children].filter(
     (el) =>
       !(el.tagName === "NAV" && el.classList.contains("pm-topnav")) &&
